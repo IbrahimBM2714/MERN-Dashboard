15afd942257be6eeab6e3939554c9326899c6bc0
IbrahimBM2714 <ibrahimbinmansoor31@gmail.com>
2025-02-04 09:39:22 +0500

    Added all the frontend features

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import styles from "@/app/ui/login/login.module.css";
 
-const Login = () => {
-  return <div>Login</div>;
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginPage;
